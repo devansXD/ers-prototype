8590fa7da5ed47a929658eb72476c1a84dae9626
devansXD <david.evans35@nhs.net>
2019-04-18 15:23:16 +0100

    analytics

--- app/assets/javascript/reveal.js
+++ app/assets/javascript/reveal.js
@@ -1,8 +1,13 @@
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+$(document).ready(function() {
+   $(".toggleitbutton1").click(function () {
+   $(".toggleitpanel1").toggle();
+  });
+
+ });
+
+$(document).ready(function() {
+   $(".toggleitbutton2").click(function () {
+   $(".toggleitpanel2").toggle();
+  });
+
+ });
