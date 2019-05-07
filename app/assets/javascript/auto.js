b72064f47105ac19e3fa9cc5fef9393494246633
devansXD <david.evans35@nhs.net>
2019-05-07 13:36:31 +0100

    v5

--- app/assets/javascript/auto.js
+++ app/assets/javascript/auto.js
@@ -2,6 +2,8 @@ accessibleAutocomplete.enhanceSelectElement({
  showNoOptionsFound: true,
  defaultValue: '',
  showAllValues: false,
+ autoselect: true,
  placeholder: "Search by specialty, clinical term or named clinician",
  selectElement: document.querySelector('#autoselect')
+
 })
--- app/assets/javascript/reveal.js
+++ app/assets/javascript/reveal.js
@@ -25,3 +25,10 @@ $(document).ready(function() {
     });
 
    });
+
+   $(document).ready(function() {
+      $(".sidebarbttn").click(function () {
+      $(".sidebar").toggle();
+     });
+
+    });
