b5cc7be4f11c3b60c6b499372cdf7c3f3532c025
David Evans <davidevans@Davids-MacBook-Pro-3.local>
2019-11-18 14:24:28 +0000

    worklists + scrolling table

--- app/assets/javascript/sort.js
+++ app/assets/javascript/sort.js
@@ -30,3 +30,8 @@ $(document).ready(function() {
   });
 
 });
+
+// requires jquery library
+//jQuery(document).ready(function() {
+//   jQuery(".main-table").clone(true).appendTo('#table-scroll').addClass('clone');
+// });
